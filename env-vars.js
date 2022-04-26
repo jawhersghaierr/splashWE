a758751b5557f2965fd1b45156888880f44fcbad
z.totinov <z.totinov@tinqin.com>
2022-04-26 15:58:19 +0300

    test for remoteEntry

--- env-vars.js
+++ env-vars.js
@@ -1,2 +1,14 @@
 window.remoteUrl = 'http://10.241.25.10:8032';
 window.libsUrl = 'http://10.241.25.10:8033';
+const urls = [
+    {
+        title: 'remoteUrl',
+        url: 'http://10.241.25.10',
+        port: 8032,
+    },
+    {
+        title: 'libsUrl',
+        url: 'http://10.241.25.10',
+        port: 8033
+    }
+]
