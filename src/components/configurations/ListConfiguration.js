5b18148c3ccd2db48d1e6239152b76a994f5a300
Zdravko.Totinov <zag@gbg.bg>
2022-08-25 15:37:52 +0300

    add configurations list

--- src/components/configurations/ListConfiguration.js
+++ src/components/configurations/ListConfiguration.js
@@ -7,6 +7,7 @@ import {useGetDisciplinesQuery} from "../../services/referentielApi"
 import './configuration.scss'
 import {matchPath} from "react-router-dom";
 import {useGetConfigsQuery} from "./services/configurationsApi";
+import {RowInfo} from "./components/RowInfo";
 
 export const ListConfiguration = (props) => {
 
@@ -26,9 +27,25 @@ export const ListConfiguration = (props) => {
             {match?.params?.id}
         </Typography>
 
-        {(data && data?.facturation?.items) && data.facturation.items.map(e=><div>
-            {e.label} ::      {e.url} <br/>
-        </div>)}
+        {data &&  <div style={{display:'flex', flexDirection: 'row', flexWrap: 'wrap', width: '100%'}}>
+
+            {data && Object.keys(data).map(tConf =>
+                <div style={{
+                    backgroundColor: '#FFF',
+                    // backgroundColor: '#F6F8FC',
+                    margin: '5px',
+                    padding: '0 25px',
+                    // maxWidth: '450px',
+                    display: 'block'
+                }} key={data[tConf].code}>
+                    <h3 style={{marginBottom: '10px'}}>{data[tConf].label} config</h3>
+                    {data[tConf]?.items.map(e => <div key={e?.code}>
+                        <RowInfo label={e.label} value={e.url} border={true} />
+                    </div>)}
+                </div>
+            )}
+
+        </div>}
 
     </div>
 }
