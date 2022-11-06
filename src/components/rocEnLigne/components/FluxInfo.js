13397503369aa4a4041aeb3399661dbfaef2ea44
z.totinov <z.totinov@tinqin.com>
2022-11-06 13:46:29 +0200

    fix flux @ROC on int

--- src/components/rocEnLigne/components/FluxInfo.js
+++ src/components/rocEnLigne/components/FluxInfo.js
@@ -21,14 +21,14 @@ export const FluxInfo = ({factId, menu= null}) => {
                 <b>Flux aller</b>
             </Typography>
             {isFetching && <CircularProgress style={{margin: '100px auto'}}/>}
-            {isSuccess && data?.fluxAller && <XMLViewer xml={data.fluxAller} theme={customTheme} style={ (menu)?{overflowY:'auto'}: {maxHeight: '400px', overflowY: 'auto'} }/>}
+            {isSuccess && data?.fluxAller && <XMLViewer xml={data.fluxAller} theme={customTheme} style={ {maxHeight: '735px', overflowY: 'auto'} }/>}
         </div>
         <div style={{flex: 1, marginRight: '25px', width: '45%', maxWidth: '650px', whiteSpace: 'break-spaces',}}>
             <Typography variant="h6" noWrap component="div" sx={{color: '#003154', marginBottom: '10px'}}>
                 <b>Flux retour</b>
             </Typography>
             {isFetching && <CircularProgress style={{margin: '100px auto'}}/>}
-            {isSuccess && data?.fluxRetour && <XMLViewer xml={data.fluxRetour} theme={customTheme} style={ (menu)?{overflowY:'auto'}: {maxHeight: '400px', overflowY: 'auto'} }/>}
+            {isSuccess && data?.fluxRetour && <XMLViewer xml={data.fluxRetour} theme={customTheme} style={ {maxHeight: '735px', overflowY: 'auto'} }/>}
         </div>
     </div>
 }
