9d86f5a7eaa43d1ef930a7ab6e982817d1cd0151
z.totinov <z.totinov@tinqin.com>
2022-11-11 13:42:46 +0200

    fix minnor bugs on autocomplete @ROC

--- src/components/shared/components/AutoCompleteCustom.js
+++ src/components/shared/components/AutoCompleteCustom.js
@@ -2,25 +2,17 @@ import React, {useState, useRef, useEffect} from 'react';
 import { TextField, Autocomplete } from "@mui/material";
 import parse from 'autosuggest-highlight/parse';
 import match from 'autosuggest-highlight/match';
-import Paper from "@material-ui/core/Paper";
 
-const CustomPaper = (props) => {
-	return <Paper elevation={8} {...props} style={{position: "relative", zIndex: 99999}} />;
-};
-
-export const AutoCompleteCustom = ({options, input, meta, label, selectMsg, deSelectMsg, selectedMsg}) => {
+export const AutoCompleteCustom = ({id, options, input, meta, label, selectMsg, deSelectMsg, selectedMsg}) => {
 
 	const [focus, setFocus] = useState(false);
-	const [value, setValue] = useState([]);
 	const [checkAll, setCheckAll] = useState({title: selectMsg, value: 'all'});
 
 	const selectAll = () => {
-		setValue(options);
 		input?.onChange(options)
 		setCheckAll({title: deSelectMsg, value: 'none'})
 	};
 	const deSelectAll = () => {
-		setValue([]);
 		input?.onChange([])
 		setCheckAll({title: selectMsg, value: 'all'})
 	};
@@ -35,22 +27,23 @@ export const AutoCompleteCustom = ({options, input, meta, label, selectMsg, deSe
 	                      value={input.value}
 	                      options={[checkAll, ...options]}
 	                      getOptionLabel={(option) => option.title}
-	                      id="combo-box-demo"
-	                      // PaperComponent={CustomPaper}
-	                      sx={{ maxHeight: '20px', whiteSpace: "nowrap", textOverflow: 'ellipsis', '& .MuiInputBase-root': {
-		                      display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
-							  padding: '2px 15px !important',
-			                      '& .MuiIconButton-root': {color: '#000', backgroundColor: 'transparent' }} }}
+	                      id={id}
+	                      sx={{
+							  '& .MuiAutocomplete-input': {minWidth: '0 !important', padding: '5px !important'},
+						  }}
 	                      renderTags={(selected) => {
 		                      if (focus) return ''
 		                      if (selected.length > 1) return `${selected.length} ${selectedMsg}`
-		                      return <span style={{whiteSpace: "nowrap", overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '100%' }}> {`(${selected[0].value}) ${selected[0].title}`} </span>;
+		                      return <span style={{whiteSpace: "nowrap", overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '93%' }}> {`(${selected[0].value}) ${selected[0].title}`} </span>;
 	                      }}
 	                      renderInput={(params) => <TextField
 		                      label={label}
-		                      // variant="outlined"
-		                      // autoComplete="false"
-		                      style={{ whiteSpace: "nowrap", overflow: 'hidden', textOverflow: 'ellipsis'}}
+		                      variant="outlined"
+		                      autoComplete="false"
+							  sx={{'& .MuiButtonBase-root, .MuiIconButton-root': {
+								  backgroundColor: 'transparent !important',
+								  color: 'rgba(0, 0, 0, 0.54) !important'
+							  }}}
 		                      {...{...params, inputProps: { ...params.inputProps } }}
 	                      />}
 	                      renderOption={(props, option, { inputValue }) => {
@@ -78,15 +71,12 @@ export const AutoCompleteCustom = ({options, input, meta, label, selectMsg, deSe
 			                      } else if (newValue.find(el => el.value == 'none')) {
 				                      deSelectAll()
 			                      } else {
-				                      setValue(newValue);
 				                      input?.onChange(newValue)
 			                      }
 		                      } else if (reason === "removeOption") {
 								  setCheckAll({title: selectMsg, value: 'all'})
-			                      setValue(newValue);
 								  input?.onChange(newValue)
 		                      } else if (reason === "clear") {
-			                      setValue([]);
 								  input?.onChange([])
 								  setCheckAll({title: deSelectMsg, value: 'none'})
 		                      }
