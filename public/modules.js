73dd13d6e28c19cd55efbd99e83272abafd8c6c9
Zdravko.Totinov <zag@gbg.bg>
2023-03-30 15:06:10 +0300

    fix

--- public/modules.js
+++ public/modules.js
@@ -1,5 +1,13 @@
 module.exports = {
 	"remoteApps": {
+		"lib_ui": "http://10.241.25.20:8038/remoteEntry.js",
+		"shared_lib_ui": "http://10.241.25.20:8039/remoteEntry.js",
+		"hospi_ui": "http://10.241.25.20:8032/remoteEntry.js",
+		"payment_ui": "http://10.241.25.20:8033/remoteEntry.js",
+		"ps_ui": "http://10.241.25.20:8034/remoteEntry.js",
+		"benef_ui": "http://10.241.25.20:8035/remoteEntry.js"
+	},
+	"remoteApps1": {
 		"lib_ui": "http://10.241.25.10:8038/remoteEntry.js",
 		"shared_lib_ui": "http://10.241.25.10:8039/remoteEntry.js",
 		"hospi_ui": "http://10.241.25.10:8032/remoteEntry.js",
@@ -7,7 +15,7 @@ module.exports = {
 		"ps_ui": "http://10.241.25.10:8034/remoteEntry.js",
 		"benef_ui": "http://10.241.25.10:8035/remoteEntry.js"
 	},
-	"remoteApps1": {
+	"remoteApps2": {
 		"lib_ui": "http://localhost:8038/remoteEntry.js",
 		"shared_lib_ui": "http://localhost:8039/remoteEntry.js",
 		"ps_ui": "http://localhost:8034/remoteEntry.js",
@@ -15,7 +23,7 @@ module.exports = {
 		"hospi_ui": "http://localhost:8032/remoteEntry.js",
 		"payment_ui": "http://localhost:8033/remoteEntry.js"
 	},
-	"apiUrls": {
+	"apiUrls1": {
 		"beneficiaire": "http://10.241.25.10:8007/api/v1",
 		"configurations": "http://10.241.25.10:8015/api/v1",
 		"downloadFacture": "http://10.241.25.10:8015/api/v1",
@@ -30,6 +38,23 @@ module.exports = {
 		"fluxSelAndIdb": "http://10.241.25.10:8001/api/v1",
 		"virements": "http://10.241.25.10:8003/api/v1",
 		"refs": "http://10.241.25.10:8015/api/v1",
+		"referentiels": "http://10.241.25.20:8004/api/v1"
+	},
+	"apiUrls": {
+		"beneficiaire": "http://10.241.25.20:8007/api/v1",
+		"configurations": "http://10.241.25.20:8015/api/v1",
+		"downloadFacture": "http://10.241.25.20:8015/api/v1",
+		"downloadSelAndIdb": "http://10.241.25.20:8015/api/v1",
+		"entities": "http://10.241.25.20:8031/api/entities",
+		"factures": "http://10.241.25.20:8005/api/v1",
+		"fluxFactures": "http://10.241.25.20:8031/api/v1",
+		"intraitables": "http://10.241.25.20:8014/api/v1",
+		"paiements": "http://10.241.25.20:8003/api/v1",
+		"ps": "http://10.241.25.20:8002/api/v1",
+		"selAndIdb": "http://10.241.25.20:8001/api/v1",
+		"fluxSelAndIdb": "http://10.241.25.20:8001/api/v1",
+		"virements": "http://10.241.25.20:8003/api/v1",
+		"refs": "http://10.241.25.20:8015/api/v1",
 		"referentiels": "http://10.241.25.20:8004/api/v1"
 	}
 };
