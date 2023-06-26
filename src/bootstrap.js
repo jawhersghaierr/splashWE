24bc6f981b99b81d6be2614ecc5fc02b907051f3
Dimitar Vasilev <konsulat@gmail.com>
2023-06-26 09:07:43 +0300

    update host menu and webpack config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,12 +7,14 @@ import configurationsReducer from "./components/configurations/configurationsSli
 
 import { Typography, CssBaseline } from "@mui/material";
 import {
-  StyledEngineProvider,
-  ThemeProvider,
-  // createTheme,
+    StyledEngineProvider,
+    ThemeProvider,
+    // createTheme,
 } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 // import Divider from '@mui/material/Divider';
+
+import { RightDrawerProvider } from "shared_lib_ui/Lib/layout/drawers";
 
 import { store } from "shared_lib_ui/store";
 import { Drawer } from "shared_lib_ui/Lib/layout/drawers";
@@ -47,109 +49,102 @@ import theme from "shared_lib_ui/MUItheme";
 // });
 
 const PageDashboard = () => (
-  <Typography
-    variant="h5"
-    noWrap
-    component="div"
-    sx={{ padding: "15px 25px", color: "#003154" }}
-  >
-    <b>Dashboard Page</b>
-  </Typography>
+    <Typography variant="h5" noWrap component="div" sx={{ padding: "15px 25px", color: "#003154" }}>
+        <b>Dashboard Page</b>
+    </Typography>
 );
 
 const PSremote = () => <RemotePsApp store={store} />;
 const BenefRemote = (props) => <RemoteBenefApp store={store} {...props} />;
 const PayementRemote = () => <RemotePayementApp store={store} />;
-const HospiRemote = (props) => (
-  <RemoteHospiApp store={store} {...props} />
-);
+const HospiRemote = (props) => <RemoteHospiApp store={store} {...props} />;
 
 const ConfigurationBase = () => <Configurations store={store} />;
 const ListConfigurationBase = () => <ListConfiguration store={store} />;
-const ConfigurationDetailsByIdBase = () => (
-  <ConfigurationDetailsById store={store} />
-);
+const ConfigurationDetailsByIdBase = () => <ConfigurationDetailsById store={store} />;
 
 const App = () => {
-  const [open, setOpen] = useState(true);
-  const [shown, setShown] = useState(true);
+    const [open, setOpen] = useState(true);
+    const [shown, setShown] = useState(true);
 
-  useEffect(() => {
-    store.injectReducer("configurations", configurationsReducer);
-  }, []);
+    useEffect(() => {
+        store.injectReducer("configurations", configurationsReducer);
+    }, []);
 
-  const handleDrawer = () => {
-    setOpen(!open);
-  };
+    const handleDrawer = () => {
+        setOpen(!open);
+    };
 
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Suspense fallback="Loading...">
-            <Box sx={{ display: "flex" }}>
-              <CssBaseline />
-              {shown && (
-                <Drawer variant="permanent" open={open}>
-                  <MenuHeader open={open} handleDrawer={handleDrawer} />
-                  <HostMenu collapsed={!open} />
-                </Drawer>
-              )}
-              <Box component="main" sx={{ flexGrow: 1 }}>
-                <Switch>
-                  <Route exact path="/" component={PageDashboard} />
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <RightDrawerProvider>
+                    <BrowserRouter>
+                        <Suspense fallback="Loading...">
+                            <Box sx={{ display: "flex" }}>
+                                <CssBaseline />
+                                {shown && (
+                                    <Drawer variant="permanent" open={open}>
+                                        <MenuHeader open={open} handleDrawer={handleDrawer} />
+                                        <HostMenu collapsed={!open} />
+                                    </Drawer>
+                                )}
+                                <Box component="main" sx={{ flexGrow: 1 }}>
+                                    <Switch>
+                                        <Route exact path="/" component={PageDashboard} />
 
-                  <Route
-                    exact={true}
-                    index={true}
-                    name={"Configuration"}
-                    path="/configuration"
-                    component={ConfigurationBase}
-                  />
-                  <Route
-                    exact={true}
-                    name={"ConfigurationLists"}
-                    path="/configuration/:domain/:code"
-                    component={ListConfigurationBase}
-                  />
-                  <Route
-                    exact={true}
-                    name={"ConfigurationDetailsById"}
-                    path="/configuration/:domain/:code/:id"
-                    component={ConfigurationDetailsByIdBase}
-                  />
+                                        <Route
+                                            exact={true}
+                                            index={true}
+                                            name={"Configuration"}
+                                            path="/configuration"
+                                            component={ConfigurationBase}
+                                        />
+                                        <Route
+                                            exact={true}
+                                            name={"ConfigurationLists"}
+                                            path="/configuration/:domain/:code"
+                                            component={ListConfigurationBase}
+                                        />
+                                        <Route
+                                            exact={true}
+                                            name={"ConfigurationDetailsById"}
+                                            path="/configuration/:domain/:code/:id"
+                                            component={ConfigurationDetailsByIdBase}
+                                        />
 
-                  <Route path="/PS">
-                    <PSremote />
-                  </Route>
-                  <Route path="/beneficiaire">
-                    <BenefRemote openMenu={setOpen} />
-                  </Route>
+                                        <Route path="/PS">
+                                            <PSremote />
+                                        </Route>
+                                        <Route path="/beneficiaire">
+                                            <BenefRemote openMenu={setOpen} />
+                                        </Route>
 
-                  <Route path="/paiement">
-                    <PayementRemote />
-                  </Route>
-                  <Route path="/virements">
-                    <PayementRemote />
-                  </Route>
+                                        <Route path="/paiement">
+                                            <PayementRemote />
+                                        </Route>
+                                        <Route path="/virements">
+                                            <PayementRemote />
+                                        </Route>
 
-                  <Route
-                    path={["/intraitables", "/serviceEnLigne", "/factures"]}
-                    render={(props) => <HospiRemote menu={setShown} />}
-                  />
-                </Switch>
-              </Box>
-            </Box>
-          </Suspense>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
-  );
+                                        <Route
+                                            path={["/intraitables", "/serviceEnLigne", "/factures"]}
+                                            render={(props) => <HospiRemote menu={setShown} />}
+                                        />
+                                    </Switch>
+                                </Box>
+                            </Box>
+                        </Suspense>
+                    </BrowserRouter>
+                </RightDrawerProvider>
+            </ThemeProvider>
+        </Provider>
+    );
 };
 
 ReactDOM.render(
-  <StyledEngineProvider injectFirst>
-    <App />
-  </StyledEngineProvider>,
-  document.getElementById("root")
+    <StyledEngineProvider injectFirst>
+        <App />
+    </StyledEngineProvider>,
+    document.getElementById("root")
 );
