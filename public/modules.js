dc533ac5ada98e1bc093ae7025b4ec444a8f1abd
Zdravko.Totinov <zag@gbg.bg>
2023-10-06 14:27:09 +0300

    fix some labels

--- public/modules.js
+++ public/modules.js
@@ -59,12 +59,14 @@ module.exports = {
 	},
 
 	"remoteApps": {
-		"lib_ui": "https://10.241.25.10:8038/remoteEntry.js",
-		"shared_lib_ui": "https://10.241.25.10:8039/remoteEntry.js",
-		"hospi_ui": "https://10.241.25.10:8032/remoteEntry.js",
-		"payment_ui": "https://10.241.25.10:8033/remoteEntry.js",
-		"ps_ui": "https://10.241.25.10:8034/remoteEntry.js",
-		"benef_ui": "https://10.241.25.10:8035/remoteEntry.js"
+		"shared_lib_ui": "http://localhost:8039/remoteEntry.js",
+		
+		"lib_ui": "http://localhost:8038/remoteEntry.js",
+		"hospi_ui": "http://10.241.25.20:8032/remoteEntry.js",
+		"payment_ui": "http://10.241.25.20:8033/remoteEntry.js",
+		"ps_ui": "http://10.241.25.20:8034/remoteEntry.js",
+		"benef_ui": "http://10.241.25.20:8035/remoteEntry.js"
+		
 	},
 
 	"apiUrlsLocal": {
@@ -155,20 +157,20 @@ module.exports = {
 	},
 	
 	"apiUrls": {
-		"selAndIdb": "https://int-api.viamedis.net/roc-api/v1",
-		"fluxSelAndIdb": "https://int-api.viamedis.net/roc-api/v1",
-		"ps": "https://int-api.viamedis.net/ps-api/v1",
-		// "entities": "http://10.241.25.10:8031/api/entities",
-		"virements": "https://int-api.viamedis.net/paiement-api/v1",
-		"paiements": "https://int-api.viamedis.net/paiement-api/v1",
-		"referentiels": "https://int-api.viamedis.net/referentiel-api/v1",
-		"factures": "https://int-api.viamedis.net/hospi-facturation-api/v1",
-		"beneficiaire": "https://int-api.viamedis.net/droits-beneficiaire-api/v1",
-		"fluxFactures": "https://int-api.viamedis.net/facturation-integration-api/v1",
-		"intraitables": "https://int-api.viamedis.net/facturation-integration-api/v1",
-		"configurations": "https://int-api.viamedis.net/aggregator-api/v1",
-		"refs": "https://int-api.viamedis.net/aggregator-api/v1",
-		"downloadFacture": "https://int-api.viamedis.net/aggregator-api/v1",
-		"downloadSelAndIdb": "https://int-api.viamedis.net/aggregator-api/v1"
+		"beneficiaire": "http://10.241.25.20:8007/api/v1",
+		"configurations": "http://10.241.25.20:8015/api/v1",
+		"downloadFacture": "http://10.241.25.20:8015/api/v1",
+		"downloadSelAndIdb": "http://10.241.25.20:8015/api/v1",
+		"entities": "http://10.241.25.20:8031/api/entities",
+		"factures": "http://10.241.25.20:8005/api/v1/",
+		"fluxFactures": "http://10.241.25.20:8014/api/v1",
+		"intraitables": "http://10.241.25.20:8014/api/v1",
+		"paiements": "http://10.241.25.20:8003/api/v1",
+		"ps": "http://10.241.25.20:8002/api/v1",
+		"selAndIdb": "http://10.241.25.20:8001/api/v1",
+		"fluxSelAndIdb": "http://10.241.25.20:8001/api/v1",
+		"virements": "http://10.241.25.20:8003/api/v1",
+		"refs": "http://10.241.25.20:8015/api/v1",
+		"referentiels": "http://10.241.25.20:8004/api/v1"
 	},
 };
