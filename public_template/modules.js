78b09752cf4858cc8c44862c0f8dc8ac6b475ead
Zdravko.Totinov <zag@gbg.bg>
2023-10-19 18:27:07 +0300

    fix deploy on dev

--- public_template/modules.js
+++ public_template/modules.js
@@ -3,7 +3,7 @@ module.exports = {
         "auth": {
             "clientId": "${CLIENT_ID}",
             "authority": "${AUTHORITY}",
-            "knownAuthorities": [${KNOWN_AUTHORITY}],
+            "knownAuthorities": ["${KNOWN_AUTHORITY}"],
             "redirectUri": "${REDIRECT_URI}",
             "postLogoutRedirectUri": "${POST_LOGOUT_REDIRECT_URI}"
         },
@@ -16,7 +16,7 @@ module.exports = {
         // },
     },
     "loginRequest": {
-        "scopes": "${LOGIN_REQUEST_SCOPES}",
+        "scopes": ["${LOGIN_REQUEST_SCOPES}"],
         "extraQueryParameters": {"ui_locales" : "fr-FR"}
     },
     
