823485f83088ac5b86703b1bc250d02b1416432c
Zdravko.Totinov <zag@gbg.bg>
2023-12-29 13:03:39 +0200

    fix: create Remboursment link in dashboard

--- src/components/Home.js
+++ src/components/Home.js
@@ -138,7 +138,6 @@ export default function Home() {
                     code: "INDU",
                     Icon: AddIcon,
                     href: "/remboursements/create",
-                    disabled: true,
                 },
             ],
             title: "Indu",
