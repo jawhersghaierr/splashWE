36b444df9bf55ff2bd785bdc5524e202f79fb5c9
Zdravko.Totinov <zag@gbg.bg>
2024-01-16 11:41:41 +0200

    fix: re-arrange bootstrap & routes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,25 +1,11 @@
-import React, { Suspense, useState, useEffect } from "lib_ui/react";
+import React, { useEffect } from "lib_ui/react";
 import ReactDOM from "lib_ui/react-dom";
-import { Link, Switch, Route, BrowserRouter } from "lib_ui/react-router-dom";
+
 import { Provider, useSelector } from "lib_ui/react-redux";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import theme from "shared_lib_ui/MUItheme";
 import { store } from "shared_lib_ui/store";
-import { DrawerProvider } from "shared_lib_ui/Lib/layout/drawers";
-import { NotFound } from "shared_lib_ui/Lib/components";
-import { TermsService, AccepterCookie } from "shared_lib_ui/Lib/layout";
 
-import HostMenu from "./leftMenu/HostMenu";
-import RemotePsApp from "ps_ui/RemotePsApp";
-import RemoteBenefApp from "benef_ui/RemoteBenefApp";
-import RemoteHospiApp from "hospi_ui/RemoteHospiApp";
-import RemotePayementApp from "payment_ui/RemotePayementApp";
-import RemoteIntraitablesApp from "intraitables_ui/RemoteIntraitablesApp";
-import RemoteTPSApp from "tps_ui/RemoteTPSApp";
-import RemoteInduApp from "indu_ui/RemoteInduApp";
 import { useCookies, CookiesProvider } from "react-cookie";
 
 // Msal imports
@@ -28,31 +14,10 @@ import { InteractionType, EventType } from "lib_ui/@azure-msal-browser";
 import { msalInstance } from "shared_lib_ui/auth";
 
 import { setAccount } from "shared_lib_ui/host";
-import RemoteAuthApp from "auth_ui/RemoteAuthApp";
-import Home from "./components/Home";
-import { getClaims } from "shared_lib_ui/host";
-import { isAuthenticated } from "shared_lib_ui/host";
-import { GetMultiModuleClaims } from "auth_ui/RemoteAuthApp";
 
-// IdleTimer
-import { timeout, promptBeforeIdle } from "shared_lib_ui/Lib";
-import { IdleTimerProvider, PresenceType, useIdleTimerContext } from "lib_ui/react-idle-timer";
-import { RefreshTokenPrompt } from "shared_lib_ui/auth";
-import Footer from "./components/Footer";
 import Routs from "./components/Routs";
-import { MODULES } from "./utils/consts";
 
 const App = () => {
-    const [promptOpen, setPromptOpen] = useState(false);
-
-    const [cookies] = useCookies([]);
-    const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
-    useEffect(() => {
-        if (Object.keys(cookies)?.length == 0) {
-            setOpenConfirmDialog(true);
-        }
-    }, [cookies]);
-
     useEffect(() => {
         const callbackId = msalInstance.addEventCallback(event => {
             switch (event.eventType) {
@@ -75,48 +40,16 @@ const App = () => {
         };
     }, []);
 
-    const onPromptIsIdle = () => {
-        setPromptOpen(true);
-    };
-
-    const onIdle = () => {
-        setPromptOpen(false);
-    };
-
-    const onActiveIsIdle = () => {
-        setPromptOpen(false);
-    };
-
     return (
         <Provider store={store}>
             <ThemeProvider theme={theme}>
-                <IdleTimerProvider
-                    timeout={timeout}
-                    promptBeforeIdle={promptBeforeIdle}
-                    throttle={500}
-                    onPrompt={onPromptIsIdle}
-                    onIdle={onIdle}
-                    onActive={onActiveIsIdle}>
-                    <CookiesProvider>
-                        <DrawerProvider>
-                            <BrowserRouter>
-                                <Suspense fallback="Loading...">
-                                    <MsalProvider instance={msalInstance}>
-                                        <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
-                                            <Box sx={{ display: "flex" }}>
-                                                <CssBaseline />
-                                                <HostMenu />
-                                                <AccepterCookie opened={openConfirmDialog} onClose={() => setOpenConfirmDialog(false)} />
-                                                <RefreshTokenPrompt open={promptOpen} />
-                                                <Routs />
-                                            </Box>
-                                        </MsalAuthenticationTemplate>
-                                    </MsalProvider>
-                                </Suspense>
-                            </BrowserRouter>
-                        </DrawerProvider>
-                    </CookiesProvider>
-                </IdleTimerProvider>
+                <CookiesProvider>
+                    <MsalProvider instance={msalInstance}>
+                        <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
+                            <Routs />
+                        </MsalAuthenticationTemplate>
+                    </MsalProvider>
+                </CookiesProvider>
             </ThemeProvider>
         </Provider>
     );
--- src/components/Routs.js
+++ src/components/Routs.js
@@ -1,9 +1,9 @@
 import React, { Suspense, useState, useEffect } from "lib_ui/react";
-import { Switch, Route } from "lib_ui/react-router-dom";
+import { Switch, Route, BrowserRouter } from "lib_ui/react-router-dom";
+import { DrawerProvider } from "shared_lib_ui/Lib/layout/drawers";
 import { useSelector } from "lib_ui/react-redux";
 import Box from "@mui/material/Box";
 import { NotFound } from "shared_lib_ui/Lib/components";
-import { TermsService } from "shared_lib_ui/Lib/layout";
 
 import RemotePsApp from "ps_ui/RemotePsApp";
 import RemoteBenefApp from "benef_ui/RemoteBenefApp";
@@ -19,6 +19,14 @@ import { isAuthenticated } from "shared_lib_ui/host";
 import { GetMultiModuleClaims } from "auth_ui/RemoteAuthApp";
 import { MODULES } from "../utils/consts";
 import Footer from "./Footer";
+import CssBaseline from "@mui/material/CssBaseline";
+import HostMenu from "../leftMenu/HostMenu";
+import { TermsService, AccepterCookie } from "shared_lib_ui/Lib/layout";
+import { RefreshTokenPrompt } from "shared_lib_ui/auth";
+import { useCookies } from "react-cookie";
+// IdleTimer
+import { timeout, promptBeforeIdle } from "shared_lib_ui/Lib";
+import { IdleTimerProvider, PresenceType, useIdleTimerContext } from "lib_ui/react-idle-timer";
 
 const PSremote = () => <RemotePsApp />;
 const BenefRemote = props => <RemoteBenefApp {...props} />;
@@ -31,58 +39,95 @@ const AuthRemote = () => <RemoteAuthApp />;
 
 const Routs = () => {
     const moduleClaims = useSelector(getClaims);
+    const [cookies] = useCookies([]);
+    const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
+    const [promptOpen, setPromptOpen] = useState(false);
+    const onPromptIsIdle = () => {
+        setPromptOpen(true);
+    };
+
+    const onIdle = () => {
+        setPromptOpen(false);
+    };
+
+    const onActiveIsIdle = () => {
+        setPromptOpen(false);
+    };
+    useEffect(() => {
+        if (Object.keys(cookies)?.length == 0) {
+            setOpenConfirmDialog(true);
+        }
+    }, [cookies]);
 
     return (
-        <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-                display: "flex",
-                flexDirection: "column",
-            }}>
-            {!moduleClaims?.isSuccess && <GetMultiModuleClaims modules={MODULES} />}
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path={["/psdashboard", "/ps", "/ps/profile"]}>
-                    <PSremote />
-                </Route>
+        <BrowserRouter>
+            <DrawerProvider>
+                <Suspense fallback="Loading...">
+                    <IdleTimerProvider
+                        timeout={timeout}
+                        promptBeforeIdle={promptBeforeIdle}
+                        throttle={500}
+                        onPrompt={onPromptIsIdle}
+                        onIdle={onIdle}
+                        onActive={onActiveIsIdle}>
+                        <Box sx={{ display: "flex" }}>
+                            <CssBaseline />
 
-                <Route path={["/terms", "/terms/terms-of-service", "/terms/legal-notice", "/terms/personal-data-protection-policy"]}>
-                    <TermsService />
-                </Route>
+                            <HostMenu />
 
-                <Route path={["/benefdashboard", "/beneficiaire"]}>
-                    <BenefRemote />
-                </Route>
+                            <AccepterCookie opened={openConfirmDialog} onClose={() => setOpenConfirmDialog(false)} />
 
-                <Route path={["/paiementdashboard", "/paiement", "/virements"]}>
-                    <PayementRemote />
-                </Route>
+                            <RefreshTokenPrompt open={promptOpen} />
 
-                <Route path={["/hospidashboard", "/serviceEnLigne", "/factures", "/intraitables", "/parametres"]}>
-                    <HospiRemote />
-                </Route>
+                            <Box component="main" sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+                                {!moduleClaims?.isSuccess && <GetMultiModuleClaims modules={MODULES} />}
+                                <Switch>
+                                    <Route exact path="/" component={Home} />
+                                    <Route path={["/psdashboard", "/ps", "/ps/profile"]}>
+                                        <PSremote />
+                                    </Route>
 
-                <Route path={["/tpsdashboard", "/tpsFactures/create", "/tpsFactures", "/TpAmcFluxInfo", "/tpAmcServiceEnLigne"]}>
-                    <TPSRemote />
-                </Route>
+                                    <Route path={["/terms", "/terms/terms-of-service", "/terms/legal-notice", "/terms/personal-data-protection-policy"]}>
+                                        <TermsService />
+                                    </Route>
 
-                <Route path={["/indusdashboard", "/indus", "/indus/create", "/remboursements"]}>
-                    <InduRemote />
-                </Route>
+                                    <Route path={["/benefdashboard", "/beneficiaire"]}>
+                                        <BenefRemote />
+                                    </Route>
 
-                <Route path={["/intraitablesdashboard", "/facturesintraitables", "/intraitables"]}>
-                    <IntraitablesRemote />
-                </Route>
+                                    <Route path={["/paiementdashboard", "/paiement", "/virements"]}>
+                                        <PayementRemote />
+                                    </Route>
 
-                <Route path="/auth">
-                    <AuthRemote />
-                </Route>
+                                    <Route path={["/hospidashboard", "/serviceEnLigne", "/factures", "/intraitables", "/parametres"]}>
+                                        <HospiRemote />
+                                    </Route>
 
-                <Route path={["/not-found", "*"]} component={NotFound} />
-            </Switch>
-            <Footer />
-        </Box>
+                                    <Route path={["/tpsdashboard", "/tpsFactures/create", "/tpsFactures", "/TpAmcFluxInfo", "/tpAmcServiceEnLigne"]}>
+                                        <TPSRemote />
+                                    </Route>
+
+                                    <Route path={["/indusdashboard", "/indus", "/indus/create", "/remboursements"]}>
+                                        <InduRemote />
+                                    </Route>
+
+                                    <Route path={["/intraitablesdashboard", "/facturesintraitables", "/intraitables"]}>
+                                        <IntraitablesRemote />
+                                    </Route>
+
+                                    <Route path="/auth">
+                                        <AuthRemote />
+                                    </Route>
+
+                                    <Route path={["/not-found", "*"]} component={NotFound} />
+                                </Switch>
+                                <Footer />
+                            </Box>
+                        </Box>
+                    </IdleTimerProvider>
+                </Suspense>
+            </DrawerProvider>
+        </BrowserRouter>
     );
 };
 export default Routs;
