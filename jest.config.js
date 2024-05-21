a20a4605712a630ffd60b82581d662244178a847
KristiyanHristov <hristov@vmandco.com>
2024-05-21 10:20:35 +0300

    fix: tests

--- jest.config.js
+++ jest.config.js
@@ -24,4 +24,5 @@ module.exports = {
         "^.+\\.js?$": "babel-jest",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/fileTransformer.js",
     },
+    testResultsProcessor: "jest-sonar-reporter",
 };
